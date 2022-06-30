cc20be8b89f07dfb6d52870ea59d78c4daf147d0
MS <93303181+saltishDev@users.noreply.github.com>
2022-07-01 02:06:30 +0900

    [자료구조] 알파벳 찾기

---
+++ BJ10809.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string str;
+	cin >> str;
+	int cnt[26];
+	fill_n(cnt, 26, -1);
+	for (int i = 0; i < str.size(); i++) {
+		if (cnt[str[i] - 'a'] == -1) {
+			cnt[str[i] - 'a'] = i;
+		}
+	}
+	for (int i : cnt) {
+		cout << i << ' ';
+	}
+	return 0;
+}
