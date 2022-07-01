38567bc5785002e922f06041c9a52b9beff29d39
MS <93303181+saltishDev@users.noreply.github.com>
2022-07-01 21:24:26 +0900

    [수학] 최대공약수와 최소공배수

---
+++ BJ2609.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int x, int y) {
+	int mod = x % y;
+	while (mod != 0) {
+		x = y;
+		y = mod;
+		mod = x % y;
+	}
+	return y;
+}
+
+int lcm(int x, int y) {
+	return x * y / gcd(x, y);
+}
+
+int main() {
+	int x, y;
+	cin >> x >> y;
+	cout << gcd(x, y) << '\n';
+	cout << lcm(x, y) << '\n';
+	return 0;
+}
