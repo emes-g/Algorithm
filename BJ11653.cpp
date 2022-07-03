11eb0b7bff14e2be31426cfa34e0fee61b8a748f
MS <93303181+saltishDev@users.noreply.github.com>
2022-07-04 02:56:01 +0900

    [수학] 소인수분해

--- BJ11653.cpp
+++ BJ11653.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 using namespace std;
 
+// 왜 i*i<=n까지만 검사하면 되는지에 관하여 확실히 알고 싶다.
+// 왜일까?
+
 int main() {
 	int n;
 	cin >> n;
-	for (int i = 2; n != 1; i++) {
+	for (int i = 2; i * i <= n; i++) {
 		while (n % i == 0) {
 			cout << i << '\n';
 			n /= i;
 		}
+	}
+	if (n > 1) {
+		cout << n;
 	}
 	return 0;
 }
