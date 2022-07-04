6e92d6baff6f5014ea699d74739d559875fa61ff
MS <93303181+saltishDev@users.noreply.github.com>
2022-07-04 23:09:17 +0900

    [DP] 2xn 타일링

---
+++ BJ11726.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+const int MOD = 10007;
+int main() {
+	int n;
+	cin >> n;
+	int dp[1001];
+	dp[1] = 1;
+	dp[2] = 2;
+	for (size_t i = 3; i <= n; i++) {
+		dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+	}
+	cout << dp[n];
+	return 0;
+}
