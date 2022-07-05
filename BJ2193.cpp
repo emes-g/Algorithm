444c465ef5c9f5818e842e8506b79710b02daeb4
MS <93303181+saltishDev@users.noreply.github.com>
2022-07-05 22:52:48 +0900

    [DP] 이친수 (일차원)

--- BJ2193.cpp
+++ BJ2193.cpp
@@ -2,18 +2,16 @@
 #include <vector>
 using namespace std;
 
-// dp[n][i] : 첫 번째 자리가 i인 n자리 이친수의 개수
-// 단, 0으로 시작하는 수는 이친수가 아니므로 답을 출력할 때는 사용하지 않는다.
+// dp[n] : n자리 이친수의 개수
 
 int main() {
 	int n;
 	cin >> n;
-	vector<vector<long long>> dp(n + 1, vector<long long>(2, 0));	// int dp[n+1][2]
-	fill_n(dp[1].begin(), 2, 1);
-	for (size_t i = 2; i <= n; i++) {
-		dp[i][0] = dp[i - 1][0] + dp[i - 1][1];
-		dp[i][1] = dp[i - 1][0];
+	vector<long long> dp(n + 1, 0);
+	dp[1] = dp[2] = 1;
+	for (size_t i = 3; i <= n; i++) {
+		dp[i] = dp[i - 1] + dp[i - 2];
 	}
-	cout << dp[n][1];
+	cout << dp[n];
 	return 0;
 }
