727e67acdbf56a109375f4d3992018bdc86fb048
MS <93303181+saltishDev@users.noreply.github.com>
2022-07-08 21:23:12 +0900

    [브루트 포스] 사탕 게임

---
+++ BJ3085.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+char color[4] = { 'C', 'P', 'Z', 'Y' };
+
+int checkRow(vector<vector<char>>& board, int n, int row) {
+	int res = 0;
+	
+	for (int i = 0; i < 4; i++) {
+		int cur = 0;
+		for (int j = 1; j <= n; j++) {
+			if (color[i] == board[row][j]) {
+				cur++;
+				res = max(res, cur);
+			}
+			else {
+				cur = 0;
+			}
+		}
+	}
+	return res;
+}
+
+int checkCol(vector<vector<char>>& board, int n, int col) {
+	int res = 0;
+
+	for (int i = 0; i < 4; i++) {
+		int cur = 0;
+		for (int j = 1; j <= n; j++) {
+			if (color[i] == board[j][col]) {
+				cur++;
+				res = max(res, cur);
+			}
+			else {
+				cur = 0;
+			}
+		}
+	}
+	return res;
+}
+
+int main() {
+	int n, ans = 0;
+	cin >> n;
+	vector<vector<char>> board(n + 2, vector<char>(n + 2, '0'));	// char board[n+2][n+2]
+	for (int i = 1; i <= n; i++) {
+		string str;
+		cin >> str;
+		for (int j = 1; j <= n; j++) {
+			board[i][j] = str[j - 1];
+		}
+	}
+	
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			if (board[i][j + 1] != '0') {	// 오른쪽과 비교
+				swap(board[i][j], board[i][j + 1]);
+				ans = max(ans, checkRow(board, n, i));
+				ans = max(ans, checkCol(board, n, j));
+				swap(board[i][j], board[i][j + 1]);
+			}
+			if (board[i + 1][j] != '0') {	// 아래와 비교
+				swap(board[i][j], board[i + 1][j]);
+				ans = max(ans, checkRow(board, n, i));
+				ans = max(ans, checkCol(board, n, j));
+				swap(board[i][j], board[i + 1][j]);
+			}
+			if (board[i][j + 1] != '0') {	// 왼쪽과 비교
+				swap(board[i][j], board[i][j - 1]);
+				ans = max(ans, checkRow(board, n, i));
+				ans = max(ans, checkCol(board, n, j));
+				swap(board[i][j], board[i][j - 1]);
+			}
+			if (board[i + 1][j] != '0') {	// 위와 비교
+				swap(board[i][j], board[i - 1][j]);
+				ans = max(ans, checkRow(board, n, i));
+				ans = max(ans, checkCol(board, n, j));
+				swap(board[i][j], board[i - 1][j]);
+			}
+			if (ans == n) {
+				cout << n;
+				return 0;
+			}
+		}
+	}
+	cout << ans;
+	return 0;
+}
