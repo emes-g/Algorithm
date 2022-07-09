b918b3df16b5cef22b9d8fc6e31bd523de4a5d40
MS <93303181+saltishDev@users.noreply.github.com>
2022-07-09 18:52:52 +0900

    [브루트 포스] 수 이어 쓰기 1

--- BJ1748.cpp
+++ BJ1748.cpp
@@ -3,6 +3,11 @@
 #include <string>
 using namespace std;
 
+// sum[n] : n자릿수까지 이어서 쓴 수의 자릿수
+// ex) sum[2] : '1234...101112...9899'라는 수의 자릿수
+
+// 시간복잡도 : O(상수)
+
 int main() {
 	int sum[9] = { 0 };
 	for (int i = 1; i < 9; i++) {
