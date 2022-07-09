f5d6d270b3760163cc9867705fc7030706126c3b
MS <93303181+saltishDev@users.noreply.github.com>
2022-07-09 22:28:42 +0900

    [브루트 포스] N과 M (1)

---
+++ BJ15649.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+bool check[9];
+vector<int> num;
+
+void BT(int n, int m, int level) {
+	if (level == m + 1) {
+		for (int i = 0; i < num.size(); i++) {
+			cout << num[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+	for (int i = 1; i <= n; i++) {
+		if (!check[i]) {
+			check[i] = true;
+			num.push_back(i);
+			BT(n, m, level + 1);
+			check[i] = false;
+			num.pop_back();
+		}
+	}
+}
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+	BT(n, m, 1);
+	return 0;
+}
