8954c5be6b49ec628d2618b607f8b85c474f3a1d
MS <93303181+saltishDev@users.noreply.github.com>
2022-07-10 03:25:10 +0900

    [브루트 포스] N과 M (5)

---
+++ BJ15654.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+bool check[9];
+int num[9], input[9];
+
+void BT(int level, int n, int m) {
+	if (level == m + 1) {
+		for (int i = 1; i <= m; i++) {
+			cout << num[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+	for (int i = 1; i <= n; i++) {
+		if (!check[i]) {
+			check[i] = true;
+			num[level] = input[i];
+			BT(level + 1, n, m);
+			check[i] = false;
+		}
+	}
+}
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) {
+		cin >> input[i];
+	}
+	sort(input + 1, input + n + 1);
+	BT(1, n, m);
+	return 0;
+}
