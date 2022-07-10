a1fdb6982fb5dfa8aa5a81a1337be95fe92d1cf7
MS <93303181+saltishDev@users.noreply.github.com>
2022-07-10 22:35:40 +0900

    [브루트 포스] 외판원 순회 2 (순열)

--- BJ10971_1.cpp
+++ BJ10971_1.cpp
@@ -4,7 +4,11 @@ using namespace std;
 const int MAX = 10000000;
 int a[10], w[10][10];
 
-// 시간복잡도 : O(N*N!)
+// 시간복잡도 : O(N*(N-1)!) == O(N!)
+
+// 기존과 달라진점
+// 1-2-3-4, 2-3-4-1, 3-4-1-2, 4-1-2-3 모두 동일한 cost를 지불해야 한다.
+// 즉 출발 지점을 1번째 도시로 지정해도 문제가 없다.
 
 int main() {
 	int n;
@@ -32,7 +36,7 @@ int main() {
 			}
 		}
 		ans = min(ans, sum);
-	} while (next_permutation(a, a + n));
+	} while (next_permutation(a + 1, a + n));
 
 	cout << ans;
 	return 0;
