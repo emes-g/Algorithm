31dc2587b1ee1dffba4cf67b21a9e6c6bc2b8d9a
MS <93303181+saltishDev@users.noreply.github.com>
2022-07-10 19:35:14 +0900

    [브루트 포스] 다음 순열 (내장 함수 이용)

---
+++ BJ10972_2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int a[10000];
+
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	if (next_permutation(a, a + n)) {
+		for (int i = 0; i < n; i++) {
+			cout << a[i] << ' ';
+		}
+	}
+	else {
+		cout << -1;
+	}
+	return 0;
+}
