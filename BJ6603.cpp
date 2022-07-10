036c962129f69d747ca8f55a8648e7258664fc2d
MS <93303181+saltishDev@users.noreply.github.com>
2022-07-10 22:45:24 +0900

    [브루트 포스] 로또

---
+++ BJ6603.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int a[12], num[6];
+
+void BT(int level, int index, int k) {
+	if (level == 6) {
+		for (int i = 0; i < 6; i++) {
+			cout << num[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+	else if (index >= k) {
+		return;
+	}
+	num[level] = a[index];
+	BT(level + 1, index + 1, k);
+	num[level] = 0;
+	BT(level, index + 1, k);
+}
+
+int main() {
+	while (true) {
+		int k;
+		cin >> k;
+		if (k == 0) {
+			break;
+		}
+		for (int i = 0; i < k; i++) {
+			cin >> a[i];
+		}
+		BT(0, 0, k);
+		cout << '\n';
+	}
+	return 0;
+}
