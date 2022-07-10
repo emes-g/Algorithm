2da5b2c8c273b7a29618fc97d67dda5de0494b6c
MS <93303181+saltishDev@users.noreply.github.com>
2022-07-10 23:59:26 +0900

    [브루트 포스] 퇴사 (재귀)

--- BJ14501_1.cpp
+++ BJ14501_1.cpp
@@ -4,13 +4,13 @@ int ans, t[15], p[15];
 
 // 시간복잡도 : O(2^n)
 
-void go(int profit, int day, int n) {
+void go(int day, int profit, int n) {
 	if (day > n) {
 		return;
 	}
 	else if (day != n) {
-		go(profit + p[day], day + t[day], n);
-		go(profit, day + 1, n);
+		go(day + t[day], profit + p[day], n);
+		go(day + 1, profit, n);
 	}
 	ans = max(ans, profit);
 }
