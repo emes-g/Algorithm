da69a8e2e96f2f38036e895a95f7986aac5c4384
MS <93303181+saltishDev@users.noreply.github.com>
2022-07-21 16:47:09 +0900

    [브루트 포스] N과 M (9) (수정 完)

--- BJ15663.cpp
+++ BJ15663.cpp
@@ -1,34 +1,43 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
+int a[8], num[8];
 bool check[8];
-int num[8], input[8];
+
+// input
+// 3 3
+// 1 1 2
+
+// output
+// 1 1 2
+// 1 2 1
+// 2 1 1
 
 // 기존 풀이는 대부분 맞지만 반례와 같은 특정 상황을 잡지 못한다.
-// 왜? num[level]에 있는 수가 항상 input[i]와 비교되면 안 된다.
-// 특정 레벨의 BT이 끝난 이후, 해당 레벨의 BT이 다시 호출되었을 때는 num[level]을 고려해서는 안 된다.
+// 왜? a[level]에 있는 수가 항상 num[i]와 비교되면 안 된다.
+// 특정 레벨의 go가 끝난 이후, 해당 레벨의 go가 다시 호출되었을 때는 a[level]을 고려해서는 안 된다.
 
 // 그러면 어떻게 해?
-// BT의 지역변수로써 num[level]의 기능을 할 수 있는 변수 prev를 생성한다.
-// 기능은 num[level]과 동일하지만 BT이 종료되면 지역변수이기 때문에 사라진다.
+// go의 지역변수로써 a[level]의 기능을 할 수 있는 변수 prev를 생성한다.
+// 기능은 a[level]과 동일하지만 go가 종료되면 지역변수이기 때문에 사라진다.
 
 // 만약 prev가 전역변수라면?
 // 기존의 문제가 해결되지 않을 뿐더러, 추가적인 문제가 발생할 수 있다.
 
-void BT(int level, int n, int m) {
+void go(int level, int n, int m) {
 	if (level == m) {
 		for (int i = 0; i < m; i++) {
-			cout << num[i] << ' ';
+			cout << a[i] << ' ';
 		}
 		cout << '\n';
 		return;
 	}
 	int prev = 0;
 	for (int i = 0; i < n; i++) {
-		if (!check[i] && prev != input[i]) {
+		if (!check[i] && prev != num[i]) {
 			check[i] = true;
-			prev = num[level] = input[i];
-			BT(level + 1, n, m);
+			prev = a[level] = num[i];
+			go(level + 1, n, m);
 			check[i] = false;
 		}
 	}
@@ -38,9 +47,8 @@ int main() {
 	int n, m;
 	cin >> n >> m;
 	for (int i = 0; i < n; i++) {
-		cin >> input[i];
+		cin >> num[i];
 	}
-	sort(input, input + n);
-	BT(0, n, m);
-	return 0;
+	sort(num, num + n);
+	go(0, n, m);
 }
