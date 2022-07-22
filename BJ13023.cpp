458ca4fc036558e803ea61e6611c47cc3e630ff4
MS <93303181+saltishDev@users.noreply.github.com>
2022-07-22 19:40:54 +0900

    [그래프 1] ABCDE

--- BJ13023.cpp
+++ BJ13023.cpp
@@ -2,30 +2,34 @@
 #include <vector>
 using namespace std;
 bool a[2000][2000];
-vector<int> g[2000];
+vector<vector<int>> g;
 vector<pair<int, int>> edges;
+
+// 인접 행렬	: 특정 두 정점 사이 간선의 존재 유무 → O(1)
+// 인접 리스트	: 특정 정점과 연결된 모든 정점 찾기 → O(degree)
+// 간선 리스트	: 간선 그 자체를 일차원 배열에 저장
 
 int main() {
 	// 1. input
 	int n, m;
 	cin >> n >> m;
+	g.assign(n, vector<int>());
 	for (int i = 0; i < m; i++) {
 		int from, to;
 		cin >> from >> to;
 		a[from][to] = a[to][from] = true;
 		g[from].push_back(to);
 		g[to].push_back(from);
-		edges.push_back({ from, to });
-		edges.push_back({ to, from });
+		edges.push_back({ from,to });
+		edges.push_back({ to,from });
 	}
-	m *= 2;
 
 	// 2. solve
-	for (int i = 0; i < m; i++) {
+	for (int i = 0; i < edges.size(); i++) {
 		// A -> B (간선 리스트)
 		int A = edges[i].first;
 		int B = edges[i].second;
-		for (int j = 0; j < m; j++) {
+		for (int j = 0; j < edges.size(); j++) {
 			// C -> D (간선 리스트)
 			int C = edges[j].first;
 			int D = edges[j].second;
@@ -41,10 +45,8 @@ int main() {
 				if (E == A || E == B || E == C || E == D) {
 					continue;
 				}
-				else {
-					cout << 1;
-					return 0;
-				}
+				cout << 1;
+				return 0;
 			}
 		}
 	}
