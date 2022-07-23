73784b3177c2f1b914847ab0281332aba5086703
MS <93303181+saltishDev@users.noreply.github.com>
2022-07-23 17:37:52 +0900

    [브루트 포스] 단어 수학

--- BJ1339.cpp
+++ BJ1339.cpp
@@ -1,56 +1,32 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 #include <cmath>
 using namespace std;
-vector<string> word;
-int check[26];
+int weight[26];
 
-// 테스트 케이스는 전부 해결되지만 시간 초과
-// 소요 시간 : O(26 * 100 * 10!) (최악의 경우)
+// <수정>
+// 모든 경우의 수를 순열로 해 볼 필요없이
+// 알파벳마다 가중치를 주어, 높은 가중치를 가진 알파벳에 높은 숫자를 부여한다.
 
 int main() {
 	int n, size = 0;
 	cin >> n;
-	fill_n(check, 26, -1);
 	for (int i = 0; i < n; i++) {
 		string str;
 		cin >> str;
-		word.push_back(str);
 		for (int j = 0; j < str.size(); j++) {
 			int idx = str[j] - 'A';
-			if (check[idx] == -1) {
-				check[idx] = size++;
-			}
+			weight[idx] += pow(10, str.size() - 1 - j);
 		}
 	}
 
-	vector<int> num(size);
-	for (int i = 0; i < size; i++) {
-		num[i] = 9 - i;
+	int sum = 0, num = 9;
+	sort(weight, weight + 26, greater<int>());
+	for (int i = 0; i < 26; i++) {
+		if (weight[i] == 0) {
+			cout << sum;
+			return 0;
+		}
+		sum += weight[i] * num--;
 	}
-
-	int ans = 0;
-	do {
-		// 매칭
-		int idx = 0;
-		for (int i = 0; i < 26; i++) {
-			if (check[i] != -1) {
-				check[i] = num[idx++];
-			}
-		}
-
-		// 구현
-		int sum = 0;
-		for (int i = 0; i < n; i++) {
-			int temp = 0;
-			for (int j = 0; j < word[i].size(); j++) {
-				idx = word[i][j] - 'A';
-				temp += check[idx] * pow(10, word[i].size() - 1 - j);
-			}
-			sum += temp;
-		}
-		ans = max(ans, sum);
-	} while (prev_permutation(num.begin(), num.end()));
-	cout << ans;
 }
