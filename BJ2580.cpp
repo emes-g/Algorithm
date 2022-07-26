097756183870484fb3ecbb27e0e7eebc56cace7a
MS <93303181+saltishDev@users.noreply.github.com>
2022-07-26 19:11:10 +0900

    [브루트 포스] 스도쿠 (시간 초과)

--- BJ2580.cpp
+++ BJ2580.cpp
@@ -1,65 +1,92 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
-int a[9][9];
+bool ok;
 
-// 상태복구를 하지 않으면, 유망성 검사에서 이전에 할당했던 값을 사용하게 된다.
-// 2020kb/228ms
+// 시간 초과
+// ∵ 모든 레벨에 대하여 가능한 모든 순열을 해 보기 때문
 
-void print() {
+bool check_area(vector<vector<int>>& temp, int row, int col) {
+	// 1. 세로 확인
+	bool check[10] = { 0, };
 	for (int i = 0; i < 9; i++) {
-		for (int j = 0; j < 9; j++) {
-			cout << a[i][j] << ' ';
-		}
-		cout << '\n';
-	}
-}
-
-bool good(int x, int y, int num) {	// 유망성 검사 (해당 위치에 해당 숫자가 괜찮은지)
-	for (int i = 0; i < 9; i++) {
-		if (a[x][i] == num) {	// 가로
+		int num = temp[i][col];
+		if (check[num] && num != 0) {
 			return false;
 		}
-		if (a[i][y] == num) {	// 세로
-			return false;
-		}
+		check[num] = true;
 	}
-	for (int i = x - x % 3; i < x - x % 3 + 3; i++) {	// 3 * 3
-		for (int j = y - y % 3; j < y - y % 3 + 3; j++) {
-			if (a[i][j] == num) {
+	// 2. 3*3 확인
+	fill_n(check, 10, false);
+	for (int i = row - row % 3; i < row - row % 3 + 3; i++) {
+		for (int j = col - col % 3; j < col - col % 3 + 3; j++) {
+			int num = temp[i][j];
+			if (check[num] && num != 0) {
 				return false;
 			}
+			check[num] = true;
 		}
 	}
 	return true;
 }
 
-void go(int x, int y) {
-	if (x == 9) {
-		print();
-		exit(0);
-	}
-	else if (y == 9) {
-		go(x + 1, 0);
-	}
-	if (!a[x][y]) {
-		for (int i = 1; i < 10; i++) {
-			if (good(x, y, i)) {
-				a[x][y] = i;
-				go(x, y + 1);
-				a[x][y] = 0;	// 이전 상태로 돌려주어야 한다. (상태 복구)
+void go(int level, vector<vector<int>> a) {
+	if (level == 9) {
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				cout << a[i][j] << ' ';
 			}
+			cout << '\n';
+		}
+		ok = true;
+		return;
+	}
+	// 1. 가로 줄을 읽는다.
+	bool check[10] = { 0, };
+	for (int i = 0; i < 9; i++) {
+		check[a[level][i]] = true;
+	}
+	// 2. 없는 숫자를 찾는다.
+	vector<int> empty;
+	for (int i = 1; i < 10; i++) {
+		if (!check[i]) {
+			empty.push_back(i);
 		}
 	}
-	else {
-		go(x, y + 1);
-	}
+	sort(empty.begin(), empty.end());
+	// 3. 가능한 경우를 모두 해 본다.
+	vector<vector<int>> temp(9, vector<int>(9));
+	do {
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				temp[i][j] = a[i][j];
+			}
+		}
+		int idx = 0;
+		for (int i = 0; i < 9; i++) {
+			if (!temp[level][i]) {
+				temp[level][i] = empty[idx++];
+				if (!check_area(temp, level, i)) {
+					break;
+				}
+			}
+			if (i == 8) {
+				go(level + 1, temp);
+				if (ok) {
+					return;
+				}
+			}
+		}
+	} while (next_permutation(empty.begin(), empty.end()));
 }
 
 int main() {
+	vector<vector<int>> a(9, vector<int>(9));
 	for (int i = 0; i < 9; i++) {
 		for (int j = 0; j < 9; j++) {
 			cin >> a[i][j];
 		}
 	}
-	go(0, 0);
+	go(0, a);
 }
