1bb7e71908889aead8e3bbfbd11726d961bfd1ac
MS <93303181+saltishDev@users.noreply.github.com>
2022-07-26 22:18:49 +0900

    [브루트 포스] N-Queen

--- BJ9663_2.cpp
+++ BJ9663_2.cpp
@@ -2,7 +2,13 @@
 using namespace std;
 int n, ans, a[14];
 
+// <변수>
 // a[i] : i행에 존재하는 퀸의 위치 (열)
+
+// <비교>
+// 기존 O(2)작업을 O(1)에 해결하여 시간 측면에서 좀 더 빠르다.
+// 기존(방법 1) : 5.5s
+// 신규(방법 2) : 4.5s
 
 bool good(int level) {
 	for (int i = level - 1; i >= 0; i--) {
