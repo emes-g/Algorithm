f4ab21f42ebc76485daa6efb1aecf433f73d65ef
MS <93303181+saltishDev@users.noreply.github.com>
2022-07-30 15:59:44 +0900

    [BFS] 움직이는 미로 탈출

--- BJ16954.cpp
+++ BJ16954.cpp
@@ -1,37 +1,70 @@
 #include <iostream>
 #include <queue>
-#include <tuple>
 using namespace std;
-char a[8][8], check[8][8][9];
-int dx[9] = { -1,1,0,0,-1,-1,1,1,0 };
-int dy[9] = { 0,0,-1,1,-1,1,-1,1,0 };
+char a[8][8], check[8][8];
+int dx[8] = { -1,1,0,0,-1,-1,1,1 };
+int dy[8] = { 0,0,-1,1,-1,1,-1,1 };
+struct node {
+	int x, y, obj;
+	node(int x, int y, int obj)
+		:x(x), y(y), obj(obj) {}
+};
 
-int bfs() {
-	queue<tuple<int, int, int>> q;
-	q.push({ 7,0,0 });
-	check[7][0][0] = true;
+bool bfs(int wall) {
+	queue<node> q;
+	q.push(node(7, 0, 0));
+	if (!wall) {
+		check[7][0] = true;
+	}
+	for (int i = 7; i >= 0; i--) {
+		for (int j = 0; j < 8; j++) {
+			if (a[i][j] == '#') {
+				q.push(node(i, j, 1));
+			}
+		}
+	}
 	while (!q.empty()) {
-		int x, y, t;
-		tie(x, y, t) = q.front();
+		int x = q.front().x;
+		int y = q.front().y;
+		int obj = q.front().obj;
 		q.pop();
-		if (x == 0 && y == 7) {
+		if (x == 0 && y == 7 && obj == 0) {
 			return 1;
 		}
-		for (int k = 0; k < 9; k++) {
-			int nx = x + dx[k];
-			int ny = y + dy[k];
-			int nt = min(t + 1, 8);
-			if (nx >= 0 && nx < 8 && ny >= 0 && ny < 8) {
-				if (nx - t >= 0 && a[nx - t][ny] == '#') {
-					continue;
+		if (!obj) {	// 사람
+			if (a[x][y] == '#') {
+				continue;
+			}
+			else if (wall) {	// 제자리
+				q.push(node(x, y, 0));
+			}
+			for (int k = 0; k < 8; k++) {
+				int nx = x + dx[k];
+				int ny = y + dy[k];
+				if (nx >= 0 && nx < 8 && ny >= 0 && ny < 8) {
+					if (wall) {
+						if (a[nx][ny] == '.') {
+							q.push(node(nx, ny, 0));
+						}
+					}
+					else {
+						if (a[nx][ny] == '.' && !check[nx][ny]) {
+							check[nx][ny] = true;
+							q.push(node(nx, ny, 0));
+						}
+					}
 				}
-				else if (nx - nt >= 0 && a[nx - nt][ny] == '#') {
-					continue;
-				}
-				else if (!check[nx][ny][nt]) {
-					check[nx][ny][nt] = true;
-					q.push({ nx,ny,nt });
-				}
+			}
+		}
+		else {	// 벽
+			int nx = x + 1;
+			a[x][y] = '.';
+			if (nx < 8) {
+				a[nx][y] = '#';
+				q.push(node(nx, y, 1));
+			}
+			else {
+				wall--;
 			}
 		}
 	}
@@ -39,8 +72,14 @@ int bfs() {
 }
 
 int main() {
+	int wall = 0;
 	for (int i = 0; i < 8; i++) {
 		cin >> a[i];
+		for (int j = 0; j < 8; j++) {
+			if (a[i][j] == '#') {
+				wall++;
+			}
+		}
 	}
-	cout << bfs();
+	cout << bfs(wall);
 }
