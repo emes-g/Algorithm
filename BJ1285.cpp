875b6e691245a5c8d3dcf174c9a850953fdd6a50
Minseok <93303181+emes-g@users.noreply.github.com>
2022-07-31 20:33:40 +0900

    [그리디] 동전 뒤집기 (수정 전)

--- BJ1285.cpp
+++ BJ1285.cpp
@@ -1,15 +1,86 @@
 #include <iostream>
-#include <cstring>
+#include <vector>
+#include <algorithm>
 using namespace std;
+int n;
 
-// 시간복잡도 : O(2^n * n^2)
+// <초기 구현 버전>
+// 아이디어 : 한 줄을 H로 만들고 나머지를 비교
+// '과연 한 줄을 모두 H로 만든 경우가 최선인가?'에 대한 증명 불가능
+
+// 실제로 구현 결과도 정답이 몇 개씩 늘어난 채로 출력됨
+// 가령, 답이 37이라 하면 43과 같이.
+
+void flip(vector<vector<bool>>& a, bool row, int x) {
+	if (row) {
+		for (int i = 0; i < n; i++) {
+			a[x][i] = !a[x][i];
+		}
+	}
+	else {
+		for (int i = 0; i < n; i++) {
+			a[i][x] = !a[i][x];
+		}
+	}
+}
+
+int go(vector<vector<bool>> a, bool row, int x) {
+	if (row) {
+		for (int i = 0; i < n; i++) {
+			if (a[x][i]) {
+				flip(a, false, i);
+			}
+		}
+		for (int i = 0; i < n; i++) {
+			if (i == x) {
+				continue;
+			}
+			int cnt = 0;	// t의 개수
+			for (int j = 0; j < n; j++) {
+				if (a[i][j]) {
+					cnt++;
+				}
+			}
+			if (cnt > n - cnt) {
+				flip(a, true, i);
+			}
+		}
+	}
+	else {
+		for (int i = 0; i < n; i++) {
+			if (a[i][x]) {
+				flip(a, true, i);
+			}
+		}
+		for (int i = 0; i < n; i++) {
+			if (i == x) {
+				continue;
+			}
+			int cnt = 0;	// t의 개수
+			for (int j = 0; j < n; j++) {
+				if (a[j][i]) {
+					cnt++;
+				}
+			}
+			if (cnt > n - cnt) {
+				flip(a, false, i);
+			}
+		}
+	}
+	int res = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (a[i][j]) {
+				res++;
+			}
+		}
+	}
+	return res;
+}
 
 int main() {
-	// 1. input
-	int n;
 	cin >> n;
-	bool a[20][20];
-	memset(a, false, sizeof(a));
+	vector<vector<bool>> a(n, vector<bool>(n));
 	for (int i = 0; i < n; i++) {
 		string str;
 		cin >> str;
@@ -19,39 +90,12 @@ int main() {
 			}
 		}
 	}
-	
-	// 2. sovle (bitmask & greedy)
 	int ans = n * n;
-	bool temp[20][20];
-	for (int k = 0; k < (1 << n); k++) {	// 뒤집을 행 결정
-		// 1. 원본 copy
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				temp[i][j] = a[i][j];
-			}
-		}
-		
-		// 2. 행 뒤집기
-		for (int i = 0; i < n; i++) {
-			if (k & (1 << i)) {
-				for (int j = 0; j < n; j++) {
-					temp[i][j] = !temp[i][j];
-				}
-			}
-		}
-
-		// 3. 열 뒤집기
-		int sum = 0;
-		for (int j = 0; j < n; j++) {
-			int t = 0;	// 해당 열에 있는 t의 개수
-			for (int i = 0; i < n; i++) {
-				if (temp[i][j]) {
-					t++;
-				}
-			}
-			sum += min(t, n - t);
-		}
-		ans = min(ans, sum);
+	for (int i = 0; i < n; i++) {	// 모든 행
+		ans = min(ans, go(a, true, i));
+	}
+	for (int i = 0; i < n; i++) {	// 모든 열
+		ans = min(ans, go(a, false, i));
 	}
 	cout << ans;
 }
