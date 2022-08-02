4905ccc7b86d86ca082e63da5adf1be7d2b235fe
Minseok <93303181+emes-g@users.noreply.github.com>
2022-08-02 23:16:48 +0900

    [그리디] 병든 나이트

--- BJ1783.cpp
+++ BJ1783.cpp
@@ -2,41 +2,21 @@
 using namespace std;
 
 int main() {
-	int n, m, ans = 0;
+	int n, m;
 	cin >> n >> m;
-	if (n == 1 || m == 1) {	// 이동 불가
+	if (n == 1) {	// 이동 불가
 		cout << 1;
-		return 0;
 	}
-	else if (n == 2) {	// 가로 2칸씩 이동
-		if (m >= 7) {
-			cout << 4;
-		}
-		else if (m >= 5) {
-			cout << 3;
-		}
-		else if (m >= 3) {
-			cout << 2;
-		}
-		else {
-			cout << 1;
-		}
-		return 0;
-	}
-
-	if (m >= 7) {	// 가로 1칸씩 이동 가능
-		ans += 2;
-		m -= 4;
-		ans += m;
-		cout << ans;
-	}
-	else if (m >= 4) {
-		cout << 4;
-	}
-	else if (m == 3) {
-		cout << 3;
+	else if (n == 2) {
+		// 본인이 있는 열 제외, (m-1)열 중에서 2열 단위로 움직일 수 있다.
+		cout << min(4, (m - 1) / 2 + 1);
 	}
 	else {
-		cout << 2;
+		if (m >= 7) {
+			cout << m - 2;
+		}
+		else {
+			cout << min(4, m);
+		}
 	}
 }
