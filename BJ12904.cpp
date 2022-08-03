bef1319ff233beb62f327442f10d0a891f1cf013
Minseok <93303181+emes-g@users.noreply.github.com>
2022-08-03 13:51:13 +0900

    [그리디] A와 B

--- BJ12904.cpp
+++ BJ12904.cpp
@@ -7,6 +7,11 @@ using namespace std;
 // 그러면 S->T를 만들 것이 아니라,
 // T→S를 만들어 보면 어떨까?
 
+// <깨달은 점>
+// reverse 함수는 o(n)만에 수행 가능하다.
+// string은 back 관련 기능들이 있다.
+// back(), push_back(), pop_back(), ...
+
 int main() {
 	string s, t;
 	cin >> s >> t;
