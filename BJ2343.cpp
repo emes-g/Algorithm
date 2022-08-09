6e6c0ede26d818b5099fcbc6eef4d42b44825ba8
Minseok <93303181+emes-g@users.noreply.github.com>
2022-08-09 23:47:53 +0900

    [이분 탐색] 기타 레슨

---
+++ BJ2343.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int n, m;
+vector<int> a;
+
+bool good(int mid) {
+	int cnt = 1, sum = 0;
+	for (int i = 0; i < n; i++) {
+		if (a[i] > mid) {	// 불가능한 상황
+			return false;
+		}
+		else if (sum + a[i] <= mid) {
+			sum += a[i];
+		}
+		else {
+			cnt++;
+			sum = a[i];
+		}
+	}
+	return cnt <= m;
+}
+
+int binary_search(int left, int right) {
+	while (left <= right) {
+		int mid = (left + right) / 2;
+		if (good(mid)) {	// 현재 블루레이 크기로 모든 레슨을 담을 수 있는 경우
+			right = mid - 1;	// 블루레이 크기를 최소로 하기 위해 축소
+		}
+		else {
+			left = mid + 1;
+		}
+	}
+	return left;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	cin >> n >> m;
+	a.resize(n);
+	int right = 0;
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+		right += a[i];
+	}
+	cout << binary_search(1, right);
+}
