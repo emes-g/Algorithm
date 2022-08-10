68ab6bcade275b63284b1a1a9b1773f5c026a276
Minseok <93303181+emes-g@users.noreply.github.com>
2022-08-11 01:56:06 +0900

    [사이클] Two Dots

--- BJ16929.cpp
+++ BJ16929.cpp
@@ -1,29 +1,33 @@
 #include <iostream>
-#include <utility>
+#include <vector>
+#include <cstring>
 using namespace std;
 char a[50][50];
 bool check[50][50];
-pair<int, int> k[4]{
-	{0,1}, {1,0}, {0,-1}, {-1,0}
-};
+int n, m;
+int dx[4] = { -1,1,0,0 };
+int dy[4] = { 0,0,-1,1 };
 
-// <격자 형식의 사이클 찾기 문제>
-// 기존 dfs의 cycle detection과는 확실한 차이가 있다.
+// <문제 파악>
+// 격자 형식의 cycle detection
 
-bool dfs(pair<int, int> cur, pair<int, int> prev, int n, int m) {
-	if (check[cur.first][cur.second]) {
+// 직전에 방문했던 정점으로 가지만 않는다면,
+// 크기가 4이상인 cycle이 보장됨.
+
+bool dfs(int x, int y, int px, int py) {
+	if (check[x][y]) {	// 방문했던 점에 재방문한 경우
 		return true;
 	}
-	check[cur.first][cur.second] = true;
-	for (int i = 0; i < 4; i++) {
-		int x = cur.first + k[i].first;
-		int y = cur.second + k[i].second;
-		if (x >= 0 && x < n && y >= 0 && y < m) {
-			// 다음에 방문할 칸이 직전에 방문한 칸이 아니면서
-			// 다음에 방문할 칸이 현재 칸과 색깔이 동일한 경우
-			if (!(x == prev.first && y == prev.second) && a[x][y] == a[cur.first][cur.second]) {
-				if (dfs({ x,y }, cur, n, m)) {
-					return true;
+	check[x][y] = true;
+	for (int k = 0; k < 4; k++) {
+		int nx = x + dx[k];
+		int ny = y + dy[k];
+		if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
+			if (a[nx][ny] == a[x][y]) {
+				if (!(px == nx && py == ny)) {	// 직전에 방문한 정점으로는 가지 않는다.
+					if (dfs(nx, ny, x, y)) {
+						return true;
+					}
 				}
 			}
 		}
@@ -32,25 +36,17 @@ bool dfs(pair<int, int> cur, pair<int, int> prev, int n, int m) {
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-
 	// 1. input
-	int n, m;
 	cin >> n >> m;
 	for (int i = 0; i < n; i++) {
-		string str;
-		cin >> str;
-		for (int j = 0; j < m; j++) {
-			a[i][j] = str[j];
-		}
+		cin >> a[i];
 	}
 
 	// 2. solve (dfs)
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
 			if (!check[i][j]) {
-				if (dfs({ i,j }, { -1,-1 }, n, m)) {
+				if (dfs(i, j, -1, -1)) {
 					cout << "Yes";
 					return 0;
 				}
@@ -58,5 +54,4 @@ int main() {
 		}
 	}
 	cout << "No";
-	return 0;
 }
