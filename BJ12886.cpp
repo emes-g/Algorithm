3f63fa53394e955314014f8f2a6e6bb526b5d50a
Minseok <93303181+emes-g@users.noreply.github.com>
2022-08-11 15:40:48 +0900

    [브루트 포스] 돌 그룹

--- BJ12886.cpp
+++ BJ12886.cpp
@@ -5,18 +5,22 @@ const int MAX = 1501;	// 최대 돌의 개수가 500개
 bool check[MAX][MAX];
 int sum;
 
-// 인자 3개로 dfs를 실행하면 (1500)^3이 나와서 시공간적으로 불가능
+// <알고리즘>
+// 1. 크기가 다른 2개를 고른다. (순서를 포함할 경우 3!)
+// 2. 큰 그룹에서 작은 그룹의 크기만큼 돌을 빼서 작은 그룹에 추가한다.
 
-void dfs(int a, int b) {
+// 인자 3개로 go를 실행하면 (1500)^3이 나와서 시공간적으로 불가능
+
+void go(int a, int b) {
 	check[a][b] = true;
 	int g[3] = { a, b, sum - a - b };
-	for (int i = 0; i < 3; i++) {
+	for (int i = 0; i < 3; i++) {	// 6가지 (자기 자신과 비교하는 경우 제외)
 		for (int j = 0; j < 3; j++) {
 			if (g[i] < g[j]) {
 				a = g[i] * 2;
 				b = g[j] - g[i];
 				if (!check[a][b]) {
-					dfs(a, b);
+					go(a, b);
 				}
 			}
 		}
@@ -31,6 +35,6 @@ int main() {
 		cout << 0;
 		return 0;
 	}
-	dfs(a, b);
+	go(a, b);
 	cout << (check[sum / 3][sum / 3] ? 1 : 0);
 }
