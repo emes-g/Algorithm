54a6966067833ea09f791205d9fc846c7bb0d8cb
Minseok <93303181+emes-g@users.noreply.github.com>
2022-08-25 14:55:40 +0900

    [BFS] 점프 점프

---
+++ BJ11060.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+const int MAX = 1000;
+int a[MAX], d[MAX];
+
+void bfs() {
+	fill(&d[0], &d[0] + MAX, MAX);
+	queue<int> q;
+	q.push(0);
+	d[0] = 0;
+	while (!q.empty()) {
+		int x = q.front();
+		q.pop();
+		for (int i = 1; i <= a[x]; i++) {
+			int next = x + i;
+			if (next < MAX && d[next] > d[x] + 1) {
+				q.push(next);
+				d[next] = d[x] + 1;
+			}
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	bfs();
+	cout << (d[n - 1] == MAX ? -1 : d[n - 1]);
+}
