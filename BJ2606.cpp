d7bd84ff569cf663a513093d374372372bf95f76
Minseok <93303181+emes-g@users.noreply.github.com>
2022-08-29 21:23:04 +0900

    [자료구조] 바이러스

--- BJ2606.cpp
+++ BJ2606.cpp
@@ -2,10 +2,39 @@
 #include <queue>
 #include <algorithm>
 using namespace std;
+vector<int> a[101];
+bool check[101];
+
+int bfs() {
+	int ans = -1;
+	queue<int> q;
+	q.push(1);
+	check[1] = true;
+	while (!q.empty()) {
+		int x = q.front();
+		q.pop();
+		ans++;
+		for (int next : a[x]) {
+			if (!check[next]) {
+				q.push(next);
+				check[next] = true;
+			}
+		}
+	}
+	return ans;
+}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
-	
-	
+
+	int n, k;
+	cin >> n >> k;
+	for (int i = 0; i < k; i++) {
+		int x, y;
+		cin >> x >> y;
+		a[x].push_back(y);
+		a[y].push_back(x);
+	}
+	cout << bfs();
 }
