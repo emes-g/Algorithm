1e9f8616047b55b4e9312cb1f1227ade036a8d1c
Minseok <93303181+emes-g@users.noreply.github.com>
2022-08-31 19:41:02 +0900

    [자료구조] 듣보잡

--- BJ1764.cpp
+++ BJ1764.cpp
@@ -5,10 +5,13 @@ using namespace std;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
-
+	
+	// 1. input
 	set<string> s, ans;
 	int n, m;
 	cin >> n >> m;
+	
+	// 2. solve
 	for (int i = 0; i < n + m; i++) {
 		string str;
 		cin >> str;
@@ -19,6 +22,8 @@ int main() {
 			s.insert(str);
 		}
 	}
+	
+	// 3. output
 	cout << ans.size() << '\n';
 	for (auto it = ans.begin(); it != ans.end(); it++) {
 		cout << *it << '\n';
